3b6dc0e335c421d635b7eba84a1a2b8bd073e9de
Rob McCormack <mrmccormack@gmail.com>
2014-03-11 11:30:18 -0400

    minor changes

--- ytapi.js
+++ ytapi.js
@@ -1,170 +1,179 @@
-  /*
+/*!
+ * YouTube api with Bootstrap 3  v.1 https://github.com/robmccormack/tb-youtube-api
+ * Copyright 2014 Rob McCormack.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+
+
+
+/*
 http://stackoverflow.com/questions/5194619/getting-youtube-video-information-using-javascript-jquery
 */
 
-  $(document).ready(function() {
-      var id = "vslQm7IYME4";
-      var t = 0; // time to start in seconds
+$(document).ready(function() {
+    var id = "vslQm7IYME4";
+    var t = 0; // time to start in seconds
 
-      // $('#alertmess').show().fadeOut(3000).delay(5000);
+    // $('#alertmess').show().fadeOut(3000).delay(5000);
 
-      //sleep(6000);
-      //$('#alertmess').show().fadeOut(10000);
-      //alert('NOTE: \n\nThe YouTube embed, the thumbnails are all retreived via YouTube\'s API. \n\n\n Look behind this alert box, to see the page the way it looks before receiving data from YouTube.');
+    //sleep(6000);
+    //$('#alertmess').show().fadeOut(10000);
+    //alert('NOTE: \n\nThe YouTube embed, the thumbnails are all retrieved via YouTube\'s API. \n\n\n Look behind this alert box, to see the page the way it looks before receiving data from YouTube.');
 
-      /*
+    /*
 // pass the id and the start time to function
 
 $(function() {
 setTimeout("getYouTubeInfo(id, t)", 8000);
 });
 */
-      // pause the script, to allow message to be read
+    // pause the script, to allow message to be read
 
-      myVar = setTimeout(function() {
-          getYouTubeInfo(id, t);
-      }, 5000);
-      //getYouTubeInfo(id, t);
+    myVar = setTimeout(function() {
+        getYouTubeInfo(id, t);
+    }, 5000);
+    //getYouTubeInfo(id, t);
 
-      // $('#alertmess').show().fadeOut(3000);
-
-
-  });
+    // $('#alertmess').show().fadeOut(3000);
 
 
-  $(function() {
-      // this is the jQuery function if testButton is clicked
-      $('#btnChangeVid').click(function() {
-          var id = "zJahlKPCL9g";
-          var t = 50; // time to start  in seconds
-          getYouTubeInfo(id, t);
-      }); // end  btn function
-  }); // end function
+});
 
 
-  function getYouTubeInfo(id, t) {
-
-      // enter your YouTube id here:
-      //   id = "zJahlKPCL9g";
-      //var id = "vslQm7IYME4";
-
-      var url = "http://gdata.youtube.com/feeds/api/videos/" + id + "?v=2&alt=json";
-      $.ajax({
-          url: url,
-          dataType: "jsonp",
-          success: function(data) {
-              parseresults(data)
-          }
-      });
-
-      // EMBED for iFrame
-      var frame = '<iframe title="Youtube Video Player" width="640" height="390" src="http://www.youtube.com/embed/' + id + '?fs=1&autoplay=0&loop=0&start= ' + t + '" frameborder="0" allowfullscreen style="border: 1px solid black"></iframe>';
-      $('#yt-frame').hide().html(frame).fadeIn(3000).delay(200);
+$(function() {
+    // this is the jQuery function if testButton is clicked
+    $('#btnChangeVid').click(function() {
+        var id = "zJahlKPCL9g";
+        var t = 50; // time to start  in seconds
+        getYouTubeInfo(id, t);
+    }); // end  btn function
+}); // end function
 
 
-  }
+function getYouTubeInfo(id, t) {
 
-  function parseresults(data) {
-      var title = data.entry.title.$t;
-      var link = data.entry.link[0].href;
+    // enter your YouTube id here:
+    //   id = "zJahlKPCL9g";
+    //var id = "vslQm7IYME4";
 
-      var description = data.entry.media$group.media$description.$t;
+    var url = "http://gdata.youtube.com/feeds/api/videos/" + id + "?v=2&alt=json";
+    $.ajax({
+        url: url,
+        dataType: "jsonp",
+        success: function(data) {
+            parseresults(data)
+        }
+    });
 
-      var published = data.entry.published.$t;
+    // EMBED for iFrame
+    var frame = '<iframe title="Youtube Video Player" width="640" height="390" src="http://www.youtube.com/embed/' + id + '?fs=1&autoplay=0&loop=0&start= ' + t + '" frameborder="0" allowfullscreen style="border: 1px solid black"></iframe>';
+    $('#yt-frame').hide().html(frame).fadeIn(3000).delay(200);
 
 
-      /* from JSON file, date format
+}
+
+function parseresults(data) {
+    var title = data.entry.title.$t;
+    var link = data.entry.link[0].href;
+
+    var description = data.entry.media$group.media$description.$t;
+
+    var published = data.entry.published.$t;
+
+
+    /* from JSON file, date format
         "published": {
       "$t": "2014-02-03T13:46:12.000Z"
     */
 
-      // convert the date
-      var published = new Date(published);
+    // convert the date
+    var published = new Date(published);
 
-      // thumbnail is an array, zero id default
-      var thumbnail = data.entry.media$group.media$thumbnail[0].url;
+    // thumbnail is an array, zero id default
+    var thumbnail = data.entry.media$group.media$thumbnail[0].url;
 
-      // thumbnails available
-      var thumbnail1 = data.entry.media$group.media$thumbnail[4].url;
+    // thumbnails available
+    var thumbnail1 = data.entry.media$group.media$thumbnail[4].url;
 
-      var thumbsec1 = data.entry.media$group.media$thumbnail[4].time;
-      // ??? get this working with thumb and link alert (thumbsec1);
-
-
-      var thumbnail2 = data.entry.media$group.media$thumbnail[5].url;
-      var thumbnail3 = data.entry.media$group.media$thumbnail[6].url;
+    var thumbsec1 = data.entry.media$group.media$thumbnail[4].time;
+    // ??? get this working with thumb and link alert (thumbsec1);
 
 
-
-
-      var viewcount = data.entry.yt$statistics.viewCount;
-
-      var author = data.entry.author[0].name.$t;
-      //data.entry.media$group.media$description.$t;
-      var duration = data.entry.media$group.yt$duration.seconds;
-
-      // String.prototype at bottom
-
-      duration = duration.toHHMMSS();
-
-      $('#yt-title').html(title);
-
-
-      // no work fo some reason
-      $('#yt-link').html('<a href="' + link + '" class="btn btn-danger btn-xs" >view on YouTube</a>');
-
-
-      // can we fade in?
-      //  $('#message').hide().html("You clicked on a checkbox.").fadeIn('slow');
-
-      $('.yt-description').hide().html('<b>Description</b>: ' + description).fadeIn(3000);
-      $('#yt-author').hide().html('<b>Author</b>: ' + author).fadeIn(5000);
-      $('#yt-published').html('<b>Published</b>: <small> ' + published + '</small>');
+    var thumbnail2 = data.entry.media$group.media$thumbnail[5].url;
+    var thumbnail3 = data.entry.media$group.media$thumbnail[6].url;
 
 
 
-      $('#yt-viewcount').html('<br/><b>Views</b>: ' + addCommas(viewcount));
+
+    var viewcount = data.entry.yt$statistics.viewCount;
+
+    var author = data.entry.author[0].name.$t;
+    //data.entry.media$group.media$description.$t;
+    var duration = data.entry.media$group.yt$duration.seconds;
+
+    // String.prototype at bottom
+
+    duration = duration.toHHMMSS();
+
+    $('#yt-title').html(title);
 
 
-      $('#yt-thumbnail').hide().html('<img src=" ' + thumbnail + '" alt="">').delay(2000).fadeIn(4000);
-
-      $('#yt-thumbnail1').html('<img src=" ' + thumbnail1 + '" alt="" class="img-rounded pad5">');
-      $('#yt-thumbnail2').html('<img src=" ' + thumbnail2 + '" alt="" class="img-rounded pad5">');
-      $('#yt-thumbnail3').html('<img src=" ' + thumbnail3 + '" alt="" class="img-rounded pad5">');
+    // no work fo some reason
+    $('#yt-link').html('<a href="' + link + '" class="btn btn-danger btn-xs" >view on YouTube</a>');
 
 
-      $('#yt-duration').html('<b>Duration: </b><small>' + duration + '</small>');
-  }
+    // can we fade in?
+    //  $('#message').hide().html("You clicked on a checkbox.").fadeIn('slow');
 
-  function addCommas(nStr) {
-      nStr += '';
-      x = nStr.split('.');
-      x1 = x[0];
-      x2 = x.length > 1 ? '.' + x[1] : '';
-      var rgx = /(\d+)(\d{3})/;
-      while (rgx.test(x1)) {
-          x1 = x1.replace(rgx, '$1' + ',' + '$2');
-      }
-      return x1 + x2;
-  }
+    $('.yt-description').hide().html('<b>Description</b>: ' + description).fadeIn(3000);
+    $('#yt-author').hide().html('<b>Author</b>: ' + author).fadeIn(5000);
+    $('#yt-published').html('<b>Published</b>: <small> ' + published + '</small>');
 
 
-   //http://stackoverflow.com/questions/6312993/javascript-seconds-to-time-with-format-hhmmss
-  String.prototype.toHHMMSS = function() {
-      var sec_num = parseInt(this, 10); // don't forget the second param
-      var hours = Math.floor(sec_num / 3600);
-      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-      var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-      if (hours < 10) {
-          hours = "0" + hours;
-      }
-      if (minutes < 10) {
-          minutes = "0" + minutes;
-      }
-      if (seconds < 10) {
-          seconds = "0" + seconds;
-      }
-      var time = hours + ':' + minutes + ':' + seconds;
-      return time;
-  }
+    $('#yt-viewcount').html('<br/><b>Views</b>: ' + addCommas(viewcount));
+
+
+    $('#yt-thumbnail').hide().html('<img src=" ' + thumbnail + '" alt="">').delay(2000).fadeIn(4000);
+
+    $('#yt-thumbnail1').html('<img src=" ' + thumbnail1 + '" alt="" class="img-rounded pad5">');
+    $('#yt-thumbnail2').html('<img src=" ' + thumbnail2 + '" alt="" class="img-rounded pad5">');
+    $('#yt-thumbnail3').html('<img src=" ' + thumbnail3 + '" alt="" class="img-rounded pad5">');
+
+
+    $('#yt-duration').html('<b>Duration: </b><small>' + duration + '</small>');
+}
+
+function addCommas(nStr) {
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
+
+//http://stackoverflow.com/questions/6312993/javascript-seconds-to-time-with-format-hhmmss
+String.prototype.toHHMMSS = function() {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var time = hours + ':' + minutes + ':' + seconds;
+    return time;
+}
